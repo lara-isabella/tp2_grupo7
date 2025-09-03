fa0acf1b1e818efa0f9b2ba01f9d28436c38fbd2
lara-isabella <laraisabellaregina@gmail.com>
2025-09-02 23:16:24 -0300

    Ejercicio 12 - Funcional

--- tp2_grupo7/src/ar/edu/unju/escmi/tp2/ejercicio12/Ejercicio12.java
+++ tp2_grupo7/src/ar/edu/unju/escmi/tp2/ejercicio12/Ejercicio12.java
@@ -1,5 +1,27 @@
 package ar.edu.unju.escmi.tp2.ejercicio12;
 
-public class Ejercicio12 {
+import java.util.Scanner;
 
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int edad;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Ingrese la edad de la persona (" + i + "/5): ");
+            edad = scanner.nextInt();
+
+            if (edad >= 1 && edad <= 12) {
+                System.out.println("Es un niño");
+            } else if (edad >= 13 && edad <= 20) {
+                System.out.println("Es un joven");
+            } else if (edad >= 21 && edad <= 100) {
+                System.out.println("Es un adulto");
+            } else {
+                System.out.println("Edad no permitida");
+            }
+        }
+        scanner.close();
+    }
 }
