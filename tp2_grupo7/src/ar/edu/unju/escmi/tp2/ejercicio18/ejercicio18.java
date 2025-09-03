c1c1a29e4f0cb043f063287a11d28d795a9a4201
lulipereira <lulipereira2006@gmail.com>
2025-09-03 11:11:54 -0300

    Ejercicios 17 y 18

---
+++ tp2_grupo7/src/ar/edu/unju/escmi/tp2/ejercicio18/ejercicio18.java
@@ -0,0 +1,27 @@
+package ar.edu.unju.escmi.tp2.ejercicio18;
+
+public class ejercicio18 {
+    public static void main(String[] args) {
+      
+        Integer num1 = 1000;
+        Integer num2 = 1000;
+
+        
+        System.out.println("Referencia de num1 es: " + System.identityHashCode(num1));
+        System.out.println("Referencia de num2 es: " + System.identityHashCode(num2));
+
+      
+        if (num1 == num2) {
+            System.out.println("Las referencias son iguales");
+        } else {
+            System.out.println("Las referencias no son iguales");
+        }
+
+       
+        if (num1.equals(num2)) {
+            System.out.println("Los valores son iguales");
+        } else {
+            System.out.println("Los valores no son iguales");
+        }
+    }
+}
