8600d45c2d12836a139c541701417b0e96d489c5
Yamila Quipildor <quipildor.yam@gmail.com>
2025-09-03 01:35:17 -0300

    Ejercicios 1 y 2

---
+++ tp2_grupo7/src/ar/edu/unju/escmi/tp2/ejercicio1/Ejercicio1.java
@@ -0,0 +1,45 @@
+package ar.edu.unju.escmi.tp2.ejercicio1;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        // Número de DNI
+        int dni = 47438119;
+        System.out.println("Mi número de DNI es: " + dni);
+
+        // Letra de ejemplar del DNI
+        char ejemplar = 'A';
+        System.out.println("La letra de ejemplar de mi DNI es: " + ejemplar);
+
+        // Edad de mamá/papá/abuela/abuelo
+        byte edadMama = 50;
+        System.out.println("La edad de mi mamá es: " + edadMama);
+
+        // Nombre y apellido de un prócer argentino
+        String procer = "Manuel José Joaquín del Corazón de Jesús Belgrano";
+        System.out.println("Un prócer argentino es: " + procer);
+
+        // Número posible en un bingo
+        int numeroBingo = 77;
+        System.out.println("Número en un bingo: " + numeroBingo);
+
+        // Precio de un celular
+        double precioCelular = 379999.77;
+        System.out.println("El precio de un celular es: $" + precioCelular);
+
+        // Estatura en metros
+        double estatura = 1.54;
+        System.out.println("Mi estatura es: " + estatura + " metros");
+
+        // Peso de una mascota en kilogramos
+        double pesoMascota = 10.5;
+        System.out.println("El peso de mi mascota es: " + pesoMascota + " kg");
+
+        // Fecha de inicio y fin del 2do cuatrimestre
+        String inicio2doCuatrimestre = "23/08/2025";
+        String fin2doCuatrimestre = "21/11/2025";
+        System.out.println("El 2do cuatrimestre inicia el: " + inicio2doCuatrimestre);
+        System.out.println("El 2do cuatrimestre finaliza el: " + fin2doCuatrimestre);
+    }
+
+}
---
+++ tp2_grupo7/src/ar/edu/unju/escmi/tp2/ejercicio2/Ejercicio2.java
@@ -0,0 +1,27 @@
+package ar.edu.unju.escmi.tp2.ejercicio2;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        String integrante1 = "Lara Colazo";
+        String integrante2 = "Lautaro Cari";
+        String integrante3 = "Tomas Obeida";
+        String integrante4 = "Luisana Pereira";
+        String integrante5 = "Yamila Quipildor";
+
+        int dni1 = 48240145;
+        int dni2 = 47124789;
+        int dni3 = 47001796;
+        int dni4 = 4780789;
+        int dni5 = 47438119;
+
+        // Mostrar los datos en consola
+        System.out.println(
+            integrante1 + " - " + dni1 + "\n" +
+            integrante2 + " - " + dni2 + "\n" +
+            integrante3 + " - " + dni3 + "\n" +
+            integrante4 + " - " + dni4 + "\n" +
+            integrante5 + " - " + dni5
+        );
+    }
+}
+
