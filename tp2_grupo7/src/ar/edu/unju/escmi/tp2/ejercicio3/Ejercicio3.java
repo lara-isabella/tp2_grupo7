e54117f265147da210b0442e615a703c08766872
Yamila Quipildor <quipildor.yam@gmail.com>
2025-09-03 02:00:28 -0300

    Ejercicios 3 y 4

---
+++ tp2_grupo7/src/ar/edu/unju/escmi/tp2/ejercicio3/Ejercicio3.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.escmi.tp2.ejercicio3;
+
+public class Ejercicio3 {
+public static void main(String[] args) {
+        // Código postal de la provincia de Jujuy
+        final int CODIGO_PJUJUY = 4600;
+
+        // Valor de π
+        final double PI = 3.1415926535;
+
+        // Cantidad de días de la semana
+        final int DIAS_SEMA = 7;
+
+        // Cantidad de meses en un año
+        final int MESES_ANIO = 12;
+
+        // Mostrar los valores en consola
+        System.out.println("Código postal de Jujuy: " + CODIGO_PJUJUY);
+        System.out.println("Valor de PI: " + PI);
+        System.out.println("Cantidad de días de la semana: " + DIAS_SEMA);
+        System.out.println("Cantidad de meses del año: " + MESES_ANIO);
+    }
+
+
+}
---
+++ tp2_grupo7/src/ar/edu/unju/escmi/tp2/ejercicio4/Ejercicio4.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.escmi.tp2.ejercicio4;
+
+public class Ejercicio4 {
+public static void main(String[] args) {
+        int num = 3;
+
+        double resultado = Math.pow(num * (num + 1) / 2.0, 2); 
+        System.out.println("El resultado de (num * (num + 1) / 2)^2 para num = " + num + " es: " + resultado);
+    }
+}
